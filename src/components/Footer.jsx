8e9e13107639109cfd73f40e02b67c4cfae03358
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-10-11 19:55:28 -0300

    chore: remove unused file

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function GetFooter() {
-  return (
-    <footer>
-      <small>&copy; 2023 theresnoexit development. All rights reserved.</small>
-    </footer>
-  );
-}
-
-export default GetFooter;
