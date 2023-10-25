37273c773cbbc7108c30e1c2e691002a8e9d48f4
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-10-25 12:37:09 -0300

    chore: Add link to React course in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,11 @@ function GetHeader() {
         alt="Logo of the React Javascript Framework"
       />
       <h3 className="logo-text">ReactFacts</h3>
-      <h4>React Course - Project 1</h4>
+      <h4>
+        <a href="https://www.youtube.com/watch?v=bMknfKXIFA8">React Course</a>
+        {" "}
+        - Project 1
+      </h4>
     </navbar>
   );
 }
